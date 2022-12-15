b806f165210dad8e206b92c406d4ef2d14323324
c-abeka <havelstech@gmail.com>
2022-12-15 23:24:04 +0300

    monty

---
+++ monty.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * main - opens a monty file 
+ * *argc: argument count
+ * *argv: arguments
+ * Return: EXIT_SUCCESS on success and EXIT_FAILURE on fail
+ */
+int main(int argc, char argv[])
+{
+	FILE *file =  NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	parsefile(file);
+	return (EXIT_SUCCESS);
+}
